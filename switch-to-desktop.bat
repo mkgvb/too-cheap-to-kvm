0e54e24824e53a7c42dbac9de79c58ddec69e0b1
mike gracia <themikegracia@gmail.com>
2023-01-05 21:11:35 -0500

    init

---
+++ switch-to-desktop.bat
@@ -0,0 +1,9 @@
+@REM 10 [device index] 08 10 [channel index] 00 00 00
+rem Switch MX Keys to channel 2
+.\hidapitester.exe --vidpid 046D/C548 --usage 0x0001 --usagePage 0xFF00 --open --length 7 --send-output 0x10,0x02,0x09,0x1e,0x00,0x00,0x00
+ 
+rem Switch MX Anywhere 3 to channel 2
+.\hidapitester.exe --vidpid 046D/C548 --usage 0x0001 --usagePage 0xFF00 --open --length 7 --send-output 0x10,0x01,0x0a,0x1b,0x00,0x00,0x00
+
+.\ControlMyMonitor.exe /SetValue "CJG9S" 60 9
+.\ControlMyMonitor.exe /SetValue "PX329" 60 17
