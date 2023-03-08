872e93e620f59ebdfc2c580466aa7abe779e85de
mike gracia <themikegracia@gmail.com>
2023-03-08 16:35:29 -0500

    new values

--- switch-to-laptop.bat
+++ switch-to-laptop.bat
@@ -9,10 +9,15 @@ rem Switch MX Anywhere 3 to channel 2
 @REM 6 is HDMI
 @REM 9 is DP1
 
-.\ControlMyMonitor.exe /SetValue "MONITOR\SAM0F9C\{4d36e96e-e325-11ce-bfc1-08002be10318}\0002" 60 6
+@REM changes values? newest at bottom
+.\ControlMyMonitor.exe /SetValue "MONITOR\SAM0F9C\{4d36e96e-e325-11ce-bfc1-08002be10318}\0002" 60 6 
+.\ControlMyMonitor.exe /SetValue "MONITOR\SAM0F9C\{4d36e96e-e325-11ce-bfc1-08002be10318}\0009" 60 6
 
 @REM PIXIO
 @REM 18 HDMI2 ?
 @REM 17 DP1
 
-.\ControlMyMonitor.exe /SetValue "MONITOR\PNS0329\{4d36e96e-e325-11ce-bfc1-08002be10318}\0001" 60 18
+@REM .\ControlMyMonitor.exe /SetValue "MONITOR\PNS0329\{4d36e96e-e325-11ce-bfc1-08002be10318}\0001" 60 18
+.\ControlMyMonitor.exe /SetValue "PX329" 60 18
+
+TIMEOUT 5
---
+++ test.bat
@@ -0,0 +1,2 @@
+.\ControlMyMonitor.exe /GetValue "MONITOR\SAM0F9C\{4d36e96e-e325-11ce-bfc1-08002be10318}\0009" 60
+echo %errorlevel% 
